38860ec098da9ad2f3727bdb91251d7dff38329b
Yohan Graterol <yohangraterol92@gmail.com>
2017-09-10 00:17:16 -0500

    First MVP for Ark Infra

--- ark-to-bin.sh
+++ ark-to-bin.sh
@@ -10,12 +10,12 @@ rm -rf ark && mkdir ark
 cd ark
 
 if hash git 2> /dev/null; then
-  git clone https://github.com/yograterol/ark-node
+  git clone https://github.com/yograterol/ark-node ark-repo
 else
   abort "git is required. Please install git and retry again."
 fi
 
-cd ark-node
+cd ark-repo
 
 # Install dependencies
 if hash npm 2> /dev/null; then
@@ -26,10 +26,11 @@ else
 fi
 
 if hash pkg 2> /dev/null; then
-  pkg --out-path ../ .
+  pkg --targets linux .
+  mv ark-node ../
 else
   abort "pkg is required. Please install pkg from https://github.com/zeit/pkg."
 fi
 
 # Copy Native module
-cp node_modules/libpq/build/Release/addon.node ../ && cd .. && rm -rf ark-node
+cp node_modules/libpq/build/Release/addon.node ../ && cd .. && rm -rf ark-repo
--- mainnet/entrypoint.sh
+++ mainnet/entrypoint.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+
+if [ "$DB_HOST" != 'localhost' ]; then
+  sed -i -e 's/localhost/'$DB_HOST'/g' config.mainnet.json
+fi
+
+if [ "$DB_USER" != 'root' ]; then
+  sed -i -e 's/"user": null,/"user": "'$DB_USER'",/g' config.mainnet.json
+fi
+
+if [ "$DB_PASSWORD" != 'password' ]; then
+  sed -i -e 's/"password",/"'$DB_PASSWORD'","/g' config.mainnet.json
+fi
+
+exec "$@"
